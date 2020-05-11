71086f0b20717783b2f7d99394021c281192695d
Gatts011 <midnightmediamail@gmail.com>
2020-05-11 13:04:18 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+
+    <Header/>
+  
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from "../src/components/layout/Header";
+export default {
+  name:"app",
+  components:{
+    Header
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Todo list app using jsonplaceholder, axios and routing</p>
+  </div>
+</template>
